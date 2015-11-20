b5bb6df19294ee0e80aa73342e851bfcb877ef4b
Joh10 <hainauxjordan@icloud.com>
2015-11-20 09:13:58 +0100

    Tristan !!!!!!

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/HomeView.fxml"));
         primaryStage.setTitle("Myba");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
---
+++ src/View/HomeController.java
@@ -0,0 +1,8 @@
+package View;
+
+/**
+ * Created by Jo on 20/11/15 at 09:09.
+ */
+public class HomeController
+{
+}
