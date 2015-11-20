e787ab1709e9817772c73e68a8a22cbd7e8cb2df
Benjamin <loncinbenjamin@hotmail.com>
2015-11-20 10:02:42 +0100

    Fix hibernate main

--- src/HibernateMain.java
+++ src/HibernateMain.java
@@ -1,64 +1,46 @@
-import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
-import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-import java.util.Map;
-
-/**
- * Created by Benjamin on 17-11-15 at 16:35
- */
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HibernateMain
 {
-    private static final SessionFactory ourSessionFactory;
-    private static final ServiceRegistry serviceRegistry;
+    private static final Logger LOGGER = Logger.getLogger("Hibernate-Tutorial");
 
-    static
+    public static void main(String[] args)
     {
+        HibernateMain main = new HibernateMain();
+        main.run();
+    }
+
+    public void run()
+    {
+        SessionFactory sessionFactory = null;
+        Session session = null;
+
         try
         {
             Configuration configuration = new Configuration();
-            configuration.configure();
-
-            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
-        } catch (Throwable ex)
-        {
-            throw new ExceptionInInitializerError(ex);
+            configuration.configure("hibernate.cfg.xml");
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            session = sessionFactory.openSession();
         }
-    }
-
-    public static Session getSession() throws HibernateException
-    {
-        return ourSessionFactory.openSession();
-    }
-
-    public static void main(final String[] args) throws Exception
-    {
-        final Session session = getSession();
-        try
+        catch (Exception e)
         {
-            System.out.println("querying all the managed entities...");
-            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
-            for (Object key : metadataMap.keySet())
-            {
-                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
-                final String entityName = classMetadata.getEntityName();
-                final Query query = session.createQuery("from " + entityName);
-                System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list())
-                {
-                    System.out.println("  " + o);
-                }
-            }
-        } finally
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+        }
+        finally
         {
-            session.close();
+            if (session != null)
+                session.close();
+
+            if (sessionFactory != null)
+                sessionFactory.close();
         }
     }
 }
