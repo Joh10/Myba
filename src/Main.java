7daf9cf4b92de8238f86807e35283da36d49dc3e
Joh10 <hainauxjordan@icloud.com>
2015-11-20 08:56:53 +0100

    Tristan est une pute 3!

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,21 @@
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.Session;
-import org.hibernate.Query;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.metadata.ClassMetadata;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.util.Map;
+public class Main extends Application {
 
-/**
- * Created by Benjamin on 17-11-15 at 16:10
- */
-
-public class Main
-{
-    private static final SessionFactory ourSessionFactory;
-
-    static
-    {
-        try
-        {
-            ourSessionFactory = new Configuration().
-                    configure("hibernate.cfg.xml").
-                    buildSessionFactory();
-        } catch (Throwable ex)
-        {
-            throw new ExceptionInInitializerError(ex);
-        }
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+        primaryStage.setTitle("Myba");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
-    public static Session getSession() throws HibernateException
-    {
-        return ourSessionFactory.openSession();
-    }
 
-    public static void main(final String[] args) throws Exception
-    {
-        final Session session = getSession();
-        try
-        {
-            System.out.println("querying all the managed entities...");
-            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
-            for (Object key : metadataMap.keySet())
-            {
-                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
-                final String entityName = classMetadata.getEntityName();
-                final Query query = session.createQuery("from " + entityName);
-                System.out.println("executing: " + query.getQueryString());
-                for (Object o : query.list())
-                {
-                    System.out.println("  " + o);
-                }
-            }
-        } finally
-        {
-            session.close();
-        }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
